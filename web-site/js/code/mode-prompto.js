c9824a852b5cb953d9dfa42231e907213f9abe2d
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2016-10-25 23:33:42 +0800

    more docs

--- web-site/js/code/mode-prompto.js
+++ web-site/js/code/mode-prompto.js
@@ -18,7 +18,7 @@ ace.define('ace/mode/prompto_highlight_rules',["require","exports","module","ace
         );
 
         var modifiers = (
-            "abstract|enumerated|extends|mutable|native|sorted|storable"
+            "abstract|desc|descending|enumerated|extends|mutable|native|storable"
         );
 
         var operators = (
@@ -31,7 +31,7 @@ ace.define('ace/mode/prompto_highlight_rules',["require","exports","module","ace
         );
 
         var functions = (
-            "write|read|close|open|execute|invoke|pass|fetch|flush|store"
+            "write|read|close|open|execute|invoke|pass|fetch|flush|sorted|store"
         );
 
         var constants = (
