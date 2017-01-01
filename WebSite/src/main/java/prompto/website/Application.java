8cb360b472ca651874e544216c1a23762d6ec738
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-01-01 16:17:39 +0800

    bind to Prompto Server

---
+++ WebSite/src/main/java/prompto/website/Application.java
@@ -0,0 +1,22 @@
+package prompto.website;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import prompto.server.AppServer;
+
+public class Application {
+
+	public static void main(String[] args) throws Throwable {
+		List<String> argsList = new ArrayList<>(Arrays.asList(args));
+		// argsList.add("-resources");
+		// argsList.add("CodeStore.pec, DevCenter.pec");
+		argsList.add("-application");
+		argsList.add("web-site");
+		argsList.add("-version");
+		argsList.add("1.0.0");
+		AppServer.main(argsList.toArray(new String[argsList.size()]), null);
+	}
+
+}
