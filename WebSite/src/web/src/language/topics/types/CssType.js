a79005bc9abeb2546391307fca30e760742dd169
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-03-18 23:09:44 +0800

    doc for Css type

--- WebSite/src/web/src/language/topics/types/CssType.js
+++ WebSite/src/web/src/language/topics/types/CssType.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Topic from "../Topic";
-import PromptoPlayer from "../../../prompto-player/PromptoPlayer";
 
 class ThisTopic extends Topic {
 
@@ -10,8 +9,27 @@ class ThisTopic extends Topic {
 
     renderContent(topicSelected) {
         return <React.Fragment>
-            TODO
-            <PromptoPlayer sampleUrl="samples/types/date/attributes.pec" runnable={true}/>
+            <h2>Css</h2>
+
+            <p>CSS stands for <b>C</b>ascading <b>S</b>tyle <b>S</b>heet.
+                CSS is a Web standard defined <a target="_blank" rel="noopener noreferrer" href="https://www.w3.org/Style/CSS/Overview.en.html">here</a>.
+                The <code>Css</code> type is the type used for CSS expressions.<br/>
+                A CSS literal is a sequence of 1 or more CSS entries, separated by semi-colons.<br/>
+                A CSS entry comprises exactly:</p>
+                    <ul>
+                        <li>a <i>property</i> (which must be a valid CSS property)</li>
+                        <li>the <code>:</code> separator</li>
+                        <li>a <i>value</i>, which can be either of:
+                            <ul>
+                                <li>a <i>literal</i> (which must be a valid CSS value for the property)</li>
+                                <li>a Prompto expression enclosed in curly braces (which must provide a valid CSS value for the property)</li>
+                            </ul>
+                        </li>
+                    </ul>
+               <p> A Css literal must be enclosed in curly brackets.</p>
+            <pre>{String.raw`{ width: 80px; display: none } -> a simple CSS literal
+{ display: { `}<i>some_value</i> }} -> a CSS literal, where the <i>value</i> for the <i>display</i> property is determined at runtime</pre>
+
             </React.Fragment>;
     }
 
