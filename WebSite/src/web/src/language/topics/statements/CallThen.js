dc3dad1d1ce91a41ad260d70681339daea013f3c
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-03-20 07:57:03 +0800

    more docs

--- WebSite/src/web/src/language/topics/statements/CallThen.js
+++ WebSite/src/web/src/language/topics/statements/CallThen.js
@@ -15,9 +15,26 @@ class ThisTopic extends Topic {
 
     renderContent(topicSelected) {
         return <React.Fragment>
-            TODO
-            <PromptoPlayer sampleUrl="samples/types/date/attributes.pec" runnable={true}/>
-            </React.Fragment>;
+            <h2><i>Call</i> then statement</h2>
+
+            <p>A <i>Call</i> then statement is a way to execute a method on the server, return immediately, and run continuing statements locally later, once the method has finished executing on the server.<br/>
+                In its simplest form, a <i>Call</i> then statement has the form <code><i>method</i> then <i>continuation</i></code>, where <i>method</i> is a method call as described in the Methods section,
+                and <i>continuation</i> is a list of statements which are to be executed only after the method has finished executing on the server.</p>
+            <PromptoPlayer lines={10} sampleUrl="samples/statements/call-then-void.pec"/>
+                <br/>
+            <p>For methods that return a value, and if the value is needed by the continuation, a <i>Call</i> then statement has the form
+                <code><i>method</i> then with <i>value continuation</i></code>, where <i>value</i> is the name you want to give to the local variable holding the result.</p>
+            <PromptoPlayer lines={10} sampleUrl="samples/statements/call-then-value.pec"/>
+            <br/>
+
+
+
+            <h3><i>Async</i> programming</h3>
+             <p>The <i>Call</i> then technique is called <i>async</i> programming. It is useful when running code in a browser, to provide the best user experience by avoiding a non-responsive UI.<br/>
+                 Without it, the browser would be blocked until the method has completed (which could take seconds or minutes).<br/>
+                 This highlights the need to <i>never</i> make assumptions on the execution sequence of async statements.
+                 Notably, if you call multiple async statements, you should not expect that their respective continuations will be executed in the same sequence.</p>
+             </React.Fragment>;
     }
 
 }
