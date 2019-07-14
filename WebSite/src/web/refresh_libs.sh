ad55a99d2057189df42e73cebfc3e5ca6402b795
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-07-14 22:36:34 +0800

    refresh libs

--- WebSite/src/web/refresh_libs.sh
+++ WebSite/src/web/refresh_libs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cp -f ../../../../prompto-factory/CodeFactory/EditorApp/src/web/public/prompto/prompto.pec public/prompto/prompto.pec
-cp -f ../../../../prompto-factory/CodeFactory/EditorApp/src/main/resources/js/lib/require.js public/js/lib/require.js
-cp -f ../../../../prompto-factory/CodeFactory/EditorApp/src/main/resources/js/lib/prompto.core.bundle.js public/js/lib/prompto.core.bundle.js
-cp -f ../../../../prompto-factory/CodeFactory/EditorApp/src/main/resources/js/lib/prompto.core.bundle.js.gz public/js/lib/prompto.core.bundle.js.gz
+cp -f ../../../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/js/lib/prompto.core.bundle.js public/js/lib/prompto.core.bundle.js
+cp -f ../../../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/js/lib/prompto.core.bundle.js.gz public/js/lib/prompto.core.bundle.js.gz
+cp -f ../../../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/js/lib/prompto.core.bundle.js.map public/js/lib/prompto.core.bundle.js.map
 
 
