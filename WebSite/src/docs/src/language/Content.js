a0bb8b3fa37cbdbcc61ad8d96b4138400b7bbf4d
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-05 15:40:05 +0800

    remove legacy stuff

--- WebSite/src/docs/src/language/Content.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Content extends React.Component {
-
-    render() {
-        return this.props.topic.renderContent(this.props.topicSelected);
-    }
-
-}
--- WebSite/src/docs/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-const SERVER_RESOURCES_ROOT_URL = "file://‎~/Development⁩ ▸ ⁨prompto⁩/Server/src/main/resources/";
-
-module.exports = function(app) {
-    app.use("/js/lib", proxy(SERVER_RESOURCES_ROOT_URL + 'js/lib'));
-};
-
