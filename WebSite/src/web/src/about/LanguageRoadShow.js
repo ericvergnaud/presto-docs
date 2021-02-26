0743aae31e9b7a6c7b19528e4847fbccf280ddd1
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-02-26 11:44:38 +0800

    improve language road show

--- WebSite/src/web/src/about/LanguageRoadShow.js
+++ WebSite/src/web/src/about/LanguageRoadShow.js
@@ -1,9 +1,10 @@
 import React from "react";
-import {Carousel} from "react-bootstrap";
+import {Carousel, Popover} from "react-bootstrap";
 
 const imageBoxStyle = { width: "100%", height: "440px", backgroundColor: "black", display: "flex" };
 const textBoxStyle = { width: "100%", height: "160px", backgroundColor: "dimgray" };
 const imageStyle = { margin: "auto", maxWidth: "90%", maxHeight: "90%"};
+const popoverStyle = { top: "360px", left: "100px"};
 
 export default class LanguageRoadShow extends React.Component {
 
@@ -16,6 +17,7 @@ export default class LanguageRoadShow extends React.Component {
             <Carousel.Item>{ this.renderStorable1() }</Carousel.Item>
             <Carousel.Item>{ this.renderStorable2() }</Carousel.Item>
             <Carousel.Item>{ this.renderStorable3() }</Carousel.Item>
+            <Carousel.Item>{ this.renderFullStack() }</Carousel.Item>
             <Carousel.Item>{ this.renderDialect1() }</Carousel.Item>
             <Carousel.Item>{ this.renderDialect2() }</Carousel.Item>
             <Carousel.Item>{ this.renderDialect3() }</Carousel.Item>
@@ -31,7 +33,7 @@ export default class LanguageRoadShow extends React.Component {
             </div>
             <Carousel.Caption>
                 <h4>Write HTML and CSS literals in code</h4>
-                Prompto natively supports HTML literals
+                Prompto natively supports HTML literals<br/>
                 discover the power of plain CSS literals for styles used only once<br/>
                 you can combine local CSS styles to build conditional styles<br/>
             </Carousel.Caption>
@@ -79,9 +81,9 @@ export default class LanguageRoadShow extends React.Component {
             </div>
             <Carousel.Caption>
                 <h4>Call server code from your web page</h4>
-                move execution of slow running code to asynchronous server execution in one keyword<br/>
+                decide where code is executed (locally or remotely) with just one keyword<br/>
                 the executed code itself does not need to change<br/>
-                <i>(not suggesting that moving the above code to the server is required)</i><br/>
+                and you can change your mind anytime<br/>
             </Carousel.Caption>
         </>;
     }
@@ -94,7 +96,7 @@ export default class LanguageRoadShow extends React.Component {
             <div style={textBoxStyle}>
             </div>
             <Carousel.Caption>
-                <h4>Define the schema of your stored data in application code!</h4>
+                <h4>Define the schema of your stored data in application code</h4>
                 mark attributes (a.k.a. fields, columns or members) as <code>storable</code><br/>
                 mark categories (a.k.a. classes) as <code>storable</code><br/>
                 your schema is defined! (and in sync with your code)<br/>
@@ -110,7 +112,7 @@ export default class LanguageRoadShow extends React.Component {
             <div style={textBoxStyle}>
             </div>
             <Carousel.Caption>
-                <h4>Store data in one statement!</h4>
+                <h4>Store data in one statement</h4>
                 simply populate your instances, then call <code>store</code><br/>
                 you can delete and store multiple instances in one statement<br/>
                 this provides ACID consistency (if supported by the back-end data store)<br/>
@@ -126,10 +128,26 @@ export default class LanguageRoadShow extends React.Component {
             <div style={textBoxStyle}>
             </div>
             <Carousel.Caption>
-                <h4>Fetch data seamlessly from your web page</h4>
+                <h4>Fetch data directly from your web page</h4>
                 write queries in the UI, and run them asynchronously on the server<br/>
                 no more insecure string based queries: queries are checked as you code them<br/>
-                and no need to create web services for data, they're built-in<br/>
+                no need to create CRUD web services either, they're built-in<br/>
+            </Carousel.Caption>
+        </>;
+    }
+
+    renderFullStack() {
+        return <>
+            <div style={imageBoxStyle}>
+                <img src="/img/language-roadshow/full-stack.png" style={imageStyle} alt=""/>
+            </div>
+            <div style={textBoxStyle}>
+            </div>
+            <Carousel.Caption>
+                <h4>Enjoy the power of a full stack language</h4>
+                automatically share data models, business logic and queries across components<br/>
+                exchange data between components without writing code<br/>>
+                move execution from one component to another seamlessly<br/>
             </Carousel.Caption>
         </>;
     }
@@ -147,6 +165,7 @@ export default class LanguageRoadShow extends React.Component {
                 so if you favor indents over curly braces you are welcome (and conversely)<br/>
                 meet Monty, 1 of the 3 Prompto dialects<br/>
             </Carousel.Caption>
+            <Popover id="M" style={popoverStyle} placement="bottom" title="Some code in Monty dialect">colons and indents<br/>python operators: '==', '!=', 'and', 'or'<br/></Popover>
         </>;
     }
 
@@ -163,6 +182,7 @@ export default class LanguageRoadShow extends React.Component {
                 do your team mates prefer writing code using other dialects? That's ok!<br/>
                 you can switch dialect any time, the code is translated automatically<br/>
             </Carousel.Caption>
+            <Popover id="O" style={popoverStyle} placement="bottom" title="The same code in Objy dialect">curly braces, semi-colons<br/>c-style operators: '==', '!=', '&&', '||'<br/></Popover>
         </>;
     }
 
@@ -179,6 +199,7 @@ export default class LanguageRoadShow extends React.Component {
                 it requires more typing, but it's explicit about what the code means<br/>
                 that proves very convenient when you need to share business logic with non-coders<br/>
             </Carousel.Caption>
+            <Popover id="E" style={popoverStyle} placement="bottom" title="The same code in Engly dialect">colons and indents, no parenthesis<br/>natural operators: '=', '&lt;>', 'and', 'or'<br/></Popover>
         </>;
     }
 
