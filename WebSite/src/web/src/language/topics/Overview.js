4a2bd2a4617915cfa19b3a74202542577336c452
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-03-01 11:25:01 +0800

    Update content

--- WebSite/src/web/src/language/topics/Overview.js
+++ WebSite/src/web/src/language/topics/Overview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Topic from "../../components/Topic";
+import {Table} from "react-bootstrap";
 
 class ThisTopic extends Topic {
 
@@ -27,34 +28,22 @@ class ThisTopic extends Topic {
 
             <h2>Run everywhere</h2>
 
-            <p>Business applications often involve UI, server-side business logic and data storage. This currently
-                requires developing
-                in multiple languages. Prompto removes this requirement and greatly simplifies development by providing
-                a single language
-                for UI, server-side logic and data modelling.<br/>
-                This is achieved by extending existing languages rather than replace them. There is a Prompto runtime
-                for Java,
-                C#, Python, JavaScript and Swift. Prompto does not compete with these languages, but rather complements
-                them with a specific focus
-                on business data modeling and storage.<br/>
+            <p>Business applications often involve UI, server-side business logic and data storage.
+                This currently requires developing in multiple languages.
+                Prompto removes this requirement by providing a single language for UI, server-side logic and data modelling.<br/>
+                This is achieved by extending existing languages rather than replace them.
+                There is a Prompto runtime for Java, C#, Python and JavaScript.
+                Prompto does not compete with these languages, but rather complements
+                them with a unique focus on data modeling, transfer and storage.<br/>
             </p>
 
             <h2>Build and run in the cloud</h2>
 
-            <p>The Prompto language is designed for cloud deployment. A strong requirement of cloud deployment
-                is that any piece of code or configuration must be easily accessible, regardless of the physical or
-                virtual box
-                on which an application is running. Prompto achieves this by relying on a distributed code store, thus
-                removing the
-                risk inherent to file based deployment.
-            </p>
-            <p>Another strong requirement of cloud deployment is that applications must be able to run on somewhat
-                heterogeneous
-                operating systems, simply because you can't instantaneously upgrade or migrate the OS on thousands of
-                boxes.
-                This ranges from minor OS patches to different OS (Linux, Windows, MacOSX). Prompto achieves this by
-                distributing source code
-                rather than machine code, and compiling/transpiling it on demand based on the environment.
+            <p>The Prompto language is designed for cloud deployment.
+                A strong requirement of cloud deployment is that applications must be able to run on heterogeneous operating systems.
+                This is because you can't instantaneously upgrade or migrate the OS on thousands of boxes.
+                This ranges from minor OS patches to different OS (Linux, Windows, MacOSX).
+                Prompto achieves this by distributing source code rather than machine code, and compiling/transpiling it on demand based on the environment.
             </p>
 
             <h2>Easy integration</h2>
@@ -70,34 +59,69 @@ class ThisTopic extends Topic {
 
             <h2>Interpreted <i>and</i> compiled</h2>
 
-            <p>Prompto runs in both interpreted and compiled mode.<br/>
-                There are good reasons for supporting both interpreted and compiled mode:
+            <p>Prompto runs in interpreted, compiled and transpiled mode.<br/>
+                There are good reasons for supporting these 3 modes:
             </p>
             <ul>
+                <li>Compiled code runs much faster, it is the recommended server execution mode in production</li>
+                <li>Transpiled code is mandatory for running in a browser.
                 <li>In scripting mode, and in the REPL, code is executed line by line, it cannot be compiled.</li>
                 <li>Interpreted code can be modified without restarting the application, which makes development
-                    very productive.
-                </li>
-                <li>The interpreter is able to run incorrect code, notably tests, which can be useful during
+                    very productive. The interpreter is able to run incorrect code, notably tests, which can be useful during
                     development, while incorrect code will fail to compile.
                 </li>
-                <li>Transpiled code (in Python and JavaScript) is sometimes hard to debug because it does not always
-                    map exactly to Prompto code.
                 </li>
             </ul>
-            <p>
-                The Java runtime comes with an interpreter, a compiler which is able to compile Prompto code to JVM
-                byte code, and a transpiler which is able to transpile Prompto
-                code to JavaScript code.<br/>
-                The C# runtime comes with an interpreter and a compiler which is able to compile Prompto code to CLR
-                byte code <i>(compiler not available yet)</i>.<br/>
-                The Python 2 and Python 3 runtimes come with an interpreter and a transpiler which is able to transpile
-                Prompto code to Python byte code <i>(transpiler not available yet)</i>.<br/>
-                The JavaScript runtime come withs an interpreter and a transpiler which is able to transpile Prompto
-                code to JavaScript code <i>(transpiler not available yet)</i>.<br/>
-                <i>The Swift runtime is planned but not available yet</i>.
-            </p>
-
+            <div style={{marginLeft: "30px", width: "860px"}} >
+                <Table size="sm" bordered>
+                    <thead>
+                        <tr>
+                            <th width={140}>Language</th>
+                            <th width={140}>Interpreter</th>
+                            <th width={140}>Compiler</th>
+                            <th width={140}>Transpiler</th>
+                            <th width={300}>Comments</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Java</td>
+                            <td>Available</td>
+                            <td>Available (JVM)</td>
+                            <td>n/a</td>
+                            <td>Interoperates with any JVM language</td>
+                        </tr>
+                        <tr>
+                            <td>C#</td>
+                            <td>Available</td>
+                            <td>Planned (CLR)</td>
+                            <td>n/a</td>
+                            <td>Interoperates with any CLR language</td>
+                        </tr>
+                        <tr>
+                            <td>Python 2/3</td>
+                            <td>Available</td>
+                            <td>n/a</td>
+                            <td>Planned</td>
+                            <td/>
+                        </tr>
+                        <tr>
+                            <td>JavaScript</td>
+                            <td>Available</td>
+                            <td>Planned (WebASM)</td>
+                            <td>Available</td>
+                            <td/>
+                        </tr>
+                        <tr>
+                            <td>Swift</td>
+                            <td>n/a</td>
+                            <td>n/a</td>
+                            <td>Planned</td>
+                            <td>Generated Swift code must be integrated</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
             <h2>Statically inferred types</h2>
 
             <p>Prompto uses static typing, like Java or C# do. This ensures best performance on all platforms.<br/>
@@ -156,9 +180,9 @@ class ThisTopic extends Topic {
 
             <p>To facilitate the conversation between all breeds of developers, Prompto comes with not one "language"
                 but three.<br/>
-                Prompto calls these <i>dialects</i>, as they are not independant languages, but rather 3 syntactic
+                Prompto calls these <i>dialects</i>, as they are not different languages, but rather 3 syntactic
                 variations of the same language concepts.<br/>
-                They make it very easy for any developer to adopt the Prompto platform, whatever his/her technical
+                They make it very easy for any developer to adopt the Prompto platform, regardless of his/her technical
                 background.<br/>
                 Prompto <i>dialects</i> seamlessly translate to one another.<br/>
                     By doing so, they eliminate the barrier of having to learn a formal grammar only to read code.<br/>
@@ -169,15 +193,13 @@ class ThisTopic extends Topic {
             <p>The Prompto language is in production. However, we are still improving the language.<br/>
                 Here are some features currently under implementation:</p>
                 <ul>
-                    <li>Support for arrow methods a.k.a lamba</li>
-                    <li>Checked widget property and state types</li>
-                    <li>Widget built-in fields such as React props and state</li>
+                    <li>Checked widget state types</li>
                     <li>Support for nested attibutes in store queries</li>
                     <li>Statistical queries such as count, sum, min, max</li>
                     <li>Projection queries that only return selected attributes rather than full fledged instances</li>
                 </ul>
                 <p>If you find that a feature is missing, <a href="mailto:support@prompto.org?subject=Feature%20request">please contact Prompto support.</a><br/>
-                    Alternately, create a <a href="https://github.com/prompto/prompto-grammars/issues">feature request on GitHub</a>.</p>
+                    Alternately, create a <a href="https://github.com/prompto/prompto-factory/issues">feature request on GitHub</a>.</p>
 
             <br/>
 
