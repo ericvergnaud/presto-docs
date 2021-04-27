f9cf40062d747291959eafe695b19b1e9ba481bb
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-04-27 12:24:03 +0200

    fix warnings

--- WebSite/src/resources/src/components/download/Download.js
+++ WebSite/src/resources/src/components/download/Download.js
@@ -6,8 +6,8 @@ export default function Download(props) {
                 <p>Disclaimer: Prompto is designed to run in the cloud. This docker image download is for evaluation and training purpose only.</p>
                 <p>Pre-requisites</p>
                 <p>You need a valid Docker and Docker-compose installation.</p>
-                <p>If you don't already have Docker unstalled, you can <a href="https://www.docker.com/products/docker-desktop" target="_blank">download it for free from here.</a></p>
-                <p>Once you have Docker installed, <a href="https://raw.githubusercontent.com/prompto/prompto-docker/master/install_prompto_macosx.sh" target="_blank">download the Prompto installer.</a></p>
+                <p>If you don't already have Docker unstalled, you can <a href="https://www.docker.com/products/docker-desktop" target="_blank" rel="noreferrer">download it for free from here.</a></p>
+                <p>Once you have Docker installed, <a href="https://raw.githubusercontent.com/prompto/prompto-docker/master/install_prompto_macosx.sh" target="_blank" rel="noreferrer">download the Prompto installer.</a></p>
                 <p>Open Terminal, and type the following:</p>
                 <p><code>mkdir ~/prompto & cd ~/prompto</code></p>
                 <p><code>mv ~/Downloads/install_prompto_macosx.sh ./</code></p>
