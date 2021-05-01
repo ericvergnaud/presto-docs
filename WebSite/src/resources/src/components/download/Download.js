7abd1e6ef03aac21edf1eaf9e779d0361fc62eff
Anshuman Mishra <anshuman7771@gmail.com>
2021-05-01 20:15:25 +0530

    added margin in reference/libraries, added jprofiler, download page

--- WebSite/src/resources/src/components/download/Download.js
+++ WebSite/src/resources/src/components/download/Download.js
@@ -1,18 +1,60 @@
+import '../../assets/css/home.scss';
+
+function download(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if(form)
+        alert("download");
+}
+
 export default function Download(props) {
     const divStyle = {display: props.visible ? "block" : "none"};
     return (
-            <article className="download" style={divStyle}>
-                <p>Installation instructions</p>
-                <p>Disclaimer: Prompto is designed to run in the cloud. This docker image download is for evaluation and training purpose only.</p>
-                <p>Pre-requisites</p>
-                <p>You need a valid Docker and Docker-compose installation.</p>
-                <p>If you don't already have Docker unstalled, you can <a href="https://www.docker.com/products/docker-desktop" target="_blank">download it for free from here.</a></p>
-                <p>Once you have Docker installed, <a href="https://raw.githubusercontent.com/prompto/prompto-docker/master/install_prompto_macosx.sh" target="_blank">download the Prompto installer.</a></p>
-                <p>Open Terminal, and type the following:</p>
-                <p><code>mkdir ~/prompto & cd ~/prompto</code></p>
-                <p><code>mv ~/Downloads/install_prompto_macosx.sh ./</code></p>
-                <p><code>chmod 777 install_prompto_macosx.sh</code></p>
-                <p><code>./install_prompto_macosx.sh</code></p>
+            <article className="p-download" style={divStyle}>
+                <div className="uk-container uk-container-medium uk-margin-medium-top">
+                    <h2>Docker Image &amp; Instructions</h2>
+
+                    <div className="uk-grid-large uk-margin-medium-top" data-uk-grid>
+                        <div className="uk-width-3-5@s">
+                            <h3>Installation Instructions</h3>
+
+                            <p className="uk-text-danger">Disclaimer: Prompto is designed to run in the cloud. This docker image download is for evaluation and training purpose only.</p>
+
+                            <h4>Pre-requisites</h4>
+                            <ul className="uk-list uk-list-disc">
+                                <li>You need a valid Docker and Docker-compose installation.</li>
+                                <li>If you don't already have Docker unstalled, you can <a href="https://www.docker.com/products/docker-desktop" target="_blank">download it for free from here.</a></li>
+                                <li>Once you have Docker installed, <a href="https://raw.githubusercontent.com/prompto/prompto-docker/master/install_prompto_macosx.sh" target="_blank">download the Prompto installer.</a></li>
+                                <li>Open Terminal, and type the following:
+                                    <pre className="uk-margin-small-top">
+                                            {`mkdir ~/prompto & cd ~/prompto
+mv ~/Downloads/install_prompto_macosx.sh ./
+chmod 777 install_prompto_macosx.sh
+./install_prompto_macosx.sh`}
+                                    </pre>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="uk-width-2-5@s">
+                            <div className="uk-background-muted uk-padding uk-margin-large-top uk-text-center">
+                                <h3>Download Docker Image</h3>
+
+                                <form className="uk-margin-medium-top uk-text-center" onSubmit={download}>
+                                    <div className="uk-margin-small-bottom">
+                                        <div className="uk-inline">
+                                            <span className="uk-form-icon" data-uk-icon="icon: mail"></span>
+                                            <input className="uk-input" type="email" placeholder="name@domain.com" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button className="uk-button uk-button-secondary p-secondary">Subscribe</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
             </article>
     );
 }
--- WebSite/src/resources/src/components/home/footer.js
+++ WebSite/src/resources/src/components/home/footer.js
@@ -1,9 +1,4 @@
-function download(event) {
-	event.preventDefault();
-	const form = event.currentTarget;
-	if(form)
-		alert("download");
-}
+import { Link } from "react-router-dom";
 
 function subscribe(event) {
 	event.preventDefault();
@@ -21,25 +16,14 @@ function Footer() {
 					<div className="uk-flex uk-flex-middle uk-flex-around">
 						<figure>	
 							<img src={ `${process.env.PUBLIC_URL}/misc/docker.png` } className="uk-width-small uk-margin-small-right" alt="docker" />
-							<span data-uk-icon="icon: download; ratio: 5;"></span>
+							<Link to="/download">
+								<span data-uk-icon="icon: download; ratio: 5;"></span>
+							</Link>
 						</figure>
 						<div>
 							<div className="uk-text-left">
 								<h2>Download the <span>DOCKER</span> image</h2>
 								<h3 className="uk-margin-remove-top">Try it out on your local machine!</h3>
-							</div>
-							<div className="uk-text-center">
-								<form onSubmit={download}>
-									<div className="uk-margin-small-bottom">
-										<div className="uk-inline">
-											<span className="uk-form-icon" data-uk-icon="icon: mail"></span>
-											<input className="uk-input" type="email" placeholder="name@domain.com" />
-										</div>
-									</div>
-									<div>
-										<button className="uk-button uk-button-secondary p-secondary">Download</button>
-									</div>
-								</form>
 							</div>
 						</div>
 					</div>
@@ -71,8 +55,17 @@ function Footer() {
 
 					<hr className="uk-margin-medium-top uk-margin-medium-bottom" />
 
-					<p className="uk-text-small"><span className="prompto">prompto</span>&nbsp;is currently running its evaluation program. On-line development and deployment tools are available on request.</p>
-					<p className="uk-text-small"><span className="prompto">prompto</span>&nbsp;is looking for contributors! If you would like to participate, please contact us - <a href="mailto:support@prompto.org?subject=Contribute%20to%20prompto&body=Hi,%20I%20would%20like%20to%20contribute%20to%20prompto">support@prompto.org</a>.</p>
+					<div className="uk-grid-medium" data-uk-grid>
+						<div className="uk-width-2-3@s">
+							<p className="uk-text-small"><span className="prompto">prompto</span>&nbsp;is currently running its evaluation program. On-line development and deployment tools are available on request.</p>
+							<p className="uk-text-small"><span className="prompto">prompto</span>&nbsp;is looking for contributors! If you would like to participate, please contact us - <a href="mailto:support@prompto.org?subject=Contribute%20to%20prompto&body=Hi,%20I%20would%20like%20to%20contribute%20to%20prompto">support@prompto.org</a>.</p>
+						</div>
+						<div className="uk-width-1-3@s">
+							<p className="uk-text-small uk-text-right">
+								The Prompto Java components are optimized using <a href="https://www.ej-technologies.com/products/jprofiler/overview.html"><img src="https://www.ej-technologies.com/images/product_banners/jprofiler_small.png" alt="JProfiler"/></a>
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</article>
