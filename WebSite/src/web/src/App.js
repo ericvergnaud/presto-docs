9d569061227ec51ab91ca1748e1f236e63ba36d1
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-05-06 12:54:54 +0200

    merge old and new web site for deployment

--- WebSite/src/web/src/App.js
+++ WebSite/src/web/src/App.js
@@ -1,64 +1,53 @@
 import React from 'react';
-import {Navbar, Nav, NavItem, Form, NavLink} from 'react-bootstrap';
-import About from "./about/About";
-import Language from "./language/Language";
-import Libraries from "./libraries/Libraries";
-import Sandbox from "./sandbox/Sandbox";
-import Tutorials from "./tutorials/Tutorials";
-import SearchBox from "./components/SearchBox";
-import {getParameterByName} from './utils/Utils';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
 
-class App extends React.Component {
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 
+import '../node_modules/uikit/dist/css/uikit.min.css';
+import '../node_modules/uikit/dist/js/uikit.min.js';
+
+import Home from "./components/home/Home";
+import Playground from "./components/playground/Playground";
+import Tutorials from "./components/tutorials/Tutorials";
+import Reference from "./components/reference/Reference";
+import Libraries from "./components/libraries/Libraries";
+import Download from "./components/download/Download";
+import Install from "./components/download/Install";
+import Navigation from "./components/navigation/Navigation";
+
+UIkit.use(Icons);
+
+export default class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {activeNav: this.getRequestedNav()};
-    }
-
-    getRequestedNav() {
-        const requested = getParameterByName("section");
-        if (requested)
-            return requested.toUpperCase();
-        else
-            return "ABOUT";
-    }
-
-    showSelectedSuggestion(suggestion) {
-        const key = suggestion.path.split("/")[0];
-        this.setState({activeNav: key}, () => this.refs[key].topicSelected(suggestion.topic));
+        this.libraries = null;
+        this.reference = null;
+        this.Navigator = withRouter(({history}) => <Navigation topicSelected={suggestion => this.topicSelected(history, suggestion)}/>);
+        this.Home = withRouter(({visible, history}) => <Home visible={visible} history={history}/>);
     }
 
     render() {
-        return <>
-            <Navbar bg="dark" variant="dark" fixed="top" >
-                <Navbar.Brand href="/">Prompto</Navbar.Brand>
-                <Nav activeKey={this.state.active} className="ml-auto" onSelect={key => this.setState({activeNav: key})}>
-                    <Form inline>
-                        <SearchBox onSelectedSuggestion={this.showSelectedSuggestion.bind(this)}/>
-                    </Form>
-                    <NavItem>
-                        <NavLink eventKey={"TUTORIALS"}>Tutorials</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink eventKey={"SANDBOX"}>Sandbox</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink eventKey={"LIBRARIES"}>Libraries</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink eventKey={"LANGUAGE"}>Reference</NavLink>
-                    </NavItem>
-                </Nav>
-            </Navbar>
-            <div className="chapter">
-                <About visible={this.state.activeNav==="ABOUT"} />
-                <Tutorials visible={this.state.activeNav==="TUTORIALS"}/>
-                <Sandbox visible={this.state.activeNav==="SANDBOX"}/>
-                <Libraries ref="LIBRARIES" visible={this.state.activeNav==="LIBRARIES"}/>
-                <Language ref="LANGUAGE" visible={this.state.activeNav==="LANGUAGE"}/>
-            </div>
-        </>;
+        return (
+            <main>
+                <Router basename="">
+                    <this.Navigator />
+                    <Route exact path="/" children={ ({match}) => <this.Home visible={match}/> } />
+                    <Route exact path="/tutorials" children={ ({match}) => <Tutorials visible={match}/> } />
+                    <Route exact path="/playground" children={ ({match}) => <Playground visible={match}/> } />
+                    <Route exact path="/libraries" children={ ({match}) => <Libraries ref={ref => this.libraries = ref || this.libraries} visible={match}/> } />
+                    <Route exact path="/reference" children={ ({match}) => <Reference ref={ref => this.reference = ref || this.reference} visible={match}/> } />
+                    <Route exact path="/download" children={ ({match}) => <Download visible={match}/> } />
+                    <Route exact path="/install" children={ ({match}) => <Install visible={match}/> } />
+                </Router>
+            </main>
+        );
     }
-}
 
-export default App;
+    topicSelected(history, suggestion) {
+        const chapter = suggestion.path.split("/")[0];
+        history.push("/" + chapter);
+        this[chapter].topicSelected(suggestion.topic);
+    }
+
+}
--- WebSite/src/web/src/utils/Utils.js
+++
@@ -1,4 +0,0 @@
-export function getParameterByName(name) {
-    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-}
