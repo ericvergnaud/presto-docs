7a4bad11d57813287acf2959a919bf7469292833
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-05-18 10:31:06 +0200

    simplify

--- WebSite/src/main/java/prompto/website/Application.java
+++ WebSite/src/main/java/prompto/website/Application.java
@@ -1,20 +1,11 @@
 package prompto.website;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import prompto.server.AppServer;
 
 public class Application {
 
 	public static void main(String[] args) throws Throwable {
-		List<String> argsList = new ArrayList<>(Arrays.asList(args));
-		argsList.add("-application");
-		argsList.add("web-site");
-		argsList.add("-version");
-		argsList.add("1.0.0");
-		AppServer.main(argsList.toArray(new String[argsList.size()]));
+		AppServer.main(args);
 	}
 
 }
