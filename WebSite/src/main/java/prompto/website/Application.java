d18e98ee4852942687bf6fdde39d10013ecd5fe0
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-05-18 11:30:49 +0200

    load server code at startup

--- WebSite/src/main/java/prompto/website/Application.java
+++ WebSite/src/main/java/prompto/website/Application.java
@@ -1,11 +1,19 @@
 package prompto.website;
 
+import java.net.URL;
+import java.util.Collection;
+
+import prompto.config.IServerConfiguration;
+import prompto.libraries.Libraries;
 import prompto.server.AppServer;
 
 public class Application {
 
 	public static void main(String[] args) throws Throwable {
-		AppServer.main(args);
+		Collection<URL> urls = Libraries.getPromptoLibraries(Application.class);
+		IServerConfiguration config = AppServer.loadConfiguration(args);
+		config = config.withResourceURLs(urls.toArray(new URL[0]));
+		AppServer.main(config, null, null, null, null);
 	}
 
 }
