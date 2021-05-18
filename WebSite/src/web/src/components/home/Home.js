4fb709fee6c551ae2d49c5a0601722234d046f0d
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-05-18 23:01:23 +0200

    fix navigation issues

--- WebSite/src/web/src/components/home/Home.js
+++ WebSite/src/web/src/components/home/Home.js
@@ -5,13 +5,20 @@ import Footer from './Footer';
 import '../../assets/css/home.scss';
 
 export default function Home(props) {
-    const divStyle = {display: props.visible ? "block" : "none"};
-    return (
+    const searchParams = new URLSearchParams(window.location.search);
+    const section = searchParams.get("section");
+    if(section) {
+        props.history.push("/" + section);
+        return <div/>;
+    } else {
+        const divStyle = {display: props.visible ? "block" : "none"};
+        return (
             <article className="home" style={divStyle}>
-                <Header />
+                <Header/>
                 <Features history={props.history}/>
-                <Footer />
+                <Footer/>
             </article>
-    );
+        );
+    }
 }
 
