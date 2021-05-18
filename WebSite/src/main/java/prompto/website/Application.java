ce6ea91a67af96153242c96d8912854e9cf0a7eb
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-05-18 21:51:47 +0200

    load AWS KMS code at startup

--- WebSite/src/main/java/prompto/website/Application.java
+++ WebSite/src/main/java/prompto/website/Application.java
@@ -3,6 +3,7 @@ package prompto.website;
 import java.net.URL;
 import java.util.Collection;
 
+import prompto.cloud.Cloud;
 import prompto.config.IServerConfiguration;
 import prompto.libraries.Libraries;
 import prompto.server.AppServer;
@@ -10,10 +11,19 @@ import prompto.server.AppServer;
 public class Application {
 
 	public static void main(String[] args) throws Throwable {
-		Collection<URL> urls = Libraries.getPromptoLibraries(Application.class);
+		installCloudJARs();
+		Class<?> kmsClass = Class.forName("prompto.aws.KMS");
+		Collection<URL> urls = Libraries.getPromptoLibraries(Application.class, kmsClass);
 		IServerConfiguration config = AppServer.loadConfiguration(args);
 		config = config.withResourceURLs(urls.toArray(new URL[0]));
 		AppServer.main(config, null, null, null, null);
+	}
+
+	private static void installCloudJARs() throws Exception {
+		Cloud cloud = Cloud.current();
+		if(cloud==null)
+			return;
+		AppServer.installCloudJARs(cloud);
 	}
 
 }
