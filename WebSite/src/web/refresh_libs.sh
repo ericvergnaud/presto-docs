efb5ec8dd4974861a55f4d76d51abcf0819e7b1c
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-10-05 12:14:24 +0200

    reflect new structure

--- WebSite/src/web/refresh_libs.sh
+++ WebSite/src/web/refresh_libs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-cp -f ../../../../prompto-factory/CodeFactory/EditorApp/src/web/public/prompto/prompto.pec public/prompto/prompto.pec
+cp -f ../../../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/prompto/prompto.pec public/prompto/prompto.pec
 cp -f ../../../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/js/lib/prompto.core.bundle.js public/js/lib/prompto.core.bundle.js
 cp -f ../../../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/js/lib/prompto.core.bundle.js.gz public/js/lib/prompto.core.bundle.js.gz
 cp -f ../../../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/js/lib/prompto.core.bundle.js.map public/js/lib/prompto.core.bundle.js.map
+cp -f ../../../../prompto-factory/CodeFactory/CodeFactory/src/main/resources/js/lib/prompto.core.bundle.js.map.gz public/js/lib/prompto.core.bundle.js.map.gz
 
 
