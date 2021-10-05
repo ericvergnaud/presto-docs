a1f41adf3e879134ec5920e27221065a377f4af6
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-10-05 12:15:01 +0200

    Version type enhancements

--- WebSite/src/web/src/components/reference/topics/types/VersionType.js
+++ WebSite/src/web/src/components/reference/topics/types/VersionType.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Topic from "../../../content/Topic";
+import PromptoPlayer from "../../../prompto-player/PromptoPlayer";
 
 class ThisTopic extends Topic {
 
@@ -12,12 +13,36 @@ class ThisTopic extends Topic {
             <h2>Version</h2>
 
             <p>The <code>Version</code> type is a technical type often used in IT systems to provide software version.<br/>
-                A Version literal is a sequence of 1 to 4 Integers, separated by characters, optionally preceded by 'v'.<br/>
-                Each Integer must be comprised between 0 and 255.<br/>
+                A regular Version literal optionally starts with a 'v', and comprises a major number comprised between 0 and 250, a minor number comprised between 0 and 250,
+                an optional fix number comprised between 0 and 250, and an optional qualifier: 'alpha', 'beta' or 'candidate' ('release' is implicit).<br/>
+                Prompto also supports 2 special version literals: 'development' and 'latest';
                 A Version literal must be enclosed in single quotes.</p>
-                <pre><code>'v1.12.37' -> a valid version</code></pre>
+                Here are a few examples:
+                <pre><code>'v1.12.37'
+                'v1.12-alpha'
+                'development'</code></pre>
             <br/>
-            </>;
+
+            <h3>
+                Version attributes
+            </h3>
+            <p>Version values have the following attributes:</p>
+            <ul>
+                <li><code>major</code>: returns an Integer equal to the major number
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="samples/types/version/major.pec" runnable={true}/>
+                </li>
+                <li><code>minor</code>: returns an Integer equal to the minor number
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="samples/types/version/minor.pec" runnable={true}/>
+                </li>
+                <li><code>fix</code>: returns an Integer equal to the fix number
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="samples/types/version/fix.pec" runnable={true}/>
+                </li>
+                <li><code>qualifier</code>: returns a Text equal to the qualifier
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="samples/types/version/qualifier.pec" runnable={true}/>
+                </li>
+            </ul>
+
+        </>;
     }
 
 }
