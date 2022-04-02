223c2736c08b7d7431623bb8159c55add3b5ab9c
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-04-02 11:44:24 +0200

    more docs

--- WebSite/src/web/src/components/reference/topics/types/ListType.js
+++ WebSite/src/web/src/components/reference/topics/types/ListType.js
@@ -35,7 +35,42 @@ a[1] -> get item at position 1 in List referenced by variable 'a' i.e. the Integ
                 <li><code>count</code>: returns an Integer equal to the number of items in the list</li>
                 <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="samples/types/list/count.pec" runnable={true}/>
             </ul>
-           <br/>
+
+            <h3>
+                List methods
+            </h3>
+            <p>List values have the following methods:</p>
+            <ul>
+                <li><code>indexOf</code>: returns a 1-based Integer index of a value, or null if absent<p/>
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="/samples/types/list/indexOf.pec" runnable={true}/>
+                    <br/>
+                </li>
+                <li><code>removeItem</code>: removes the value located at a 1-based index in a mutable list<p/>
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="/samples/types/list/removeItem.pec" runnable={true}/>
+                    <br/>
+                </li>
+                <li><code>addValue</code>: appends a value at the end of a mutable list<p/>
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="/samples/types/list/addValue.pec" runnable={true}/>
+                    <br/>
+                </li>
+                <li><code>insertValue</code>: inserts a value at a specific index of a mutable list<p/>
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="/samples/types/list/insertValue.pec" runnable={true}/>
+                    <br/>
+                </li>
+                <li><code>removeValue</code>: removes a value from a mutable list<p/>
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="/samples/types/list/removeValue.pec" runnable={true}/>
+                    <br/>
+                </li>
+                <li><code>join</code>: returns a Text built by concatenating textual values of the list, optionally separated by a Text value<p/>
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="/samples/types/list/join.pec" runnable={true}/>
+                    <br/>
+                </li>
+                <li><code>toSet</code>: returns a Set containing all unique values<p/>
+                    <PromptoPlayer key={PromptoPlayer.nextKey()} sampleUrl="/samples/types/list/toSet.pec" runnable={true}/>
+                    <br/>
+                </li>
+            </ul>
+            <br/>
             </>;
     }
 
